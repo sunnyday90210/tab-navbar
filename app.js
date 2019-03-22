b52bbdaf8829dbde2efcd643edfb8379e98c0c46
Robert Wollny <robertjohnwollny@gmail.com>
2019-03-21 22:53:47 -0700

    :lipstick:

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const buttons = document.querySelectorAll('.btn');
-
-buttons.forEach(clickedBtn => {
-  clickedBtn.addEventListener('click', () => {
-    clickedBtn.classList.toggle('active');
-  });
-});
+function toggleItem(buttons) {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function (e) {
+      let current = this;
+      for (let i = 0; i < buttons.length; i++) {
+        if (current != buttons[i]) {
+          buttons[i].classList.remove("active");
+        } else if (current.classList.contains("active") === true) {
+          current.classList.remove("active");
+        } else {
+          current.classList.add("active");
+        }
+      }
+      e.preventDefault();
+    });
+  }
+}
+toggleItem(document.querySelectorAll(".btn"));
